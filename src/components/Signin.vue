c6d0beb808c05d6709ea5a50fcf64f27d2466036
sugawarahr <fh6edpizf@gmail.com>
2019-06-16 12:02:17 +0900

    Signページ追加, goによるサーバースクリプト追加

---
+++ src/components/Signin.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="signin">
+    <h2>Sign in</h2>
+  <input type="text" placeholder="Username" v-model="email">
+  <input type="password" placeholder="Password" v-model="password">
+  <button>Register</button>
+  <p>You don't have an account ?
+      <router-link to="/signup">create account new!!</router-link>
+  </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  margin: 10px 0;
+  padding: 10px;
+}
+</style>
---
+++ src/components/Signup.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="signup">
+    <h2>Sign up</h2>
+  <input type="text" placeholder="Username" v-model="email">
+  <input type="password" placeholder="Password" v-model="password">
+  <button>Register</button>
+  <p>Do you have an account ?
+      <router-link to="/signin">create account new!!</router-link>
+  </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+button {
+  margin: 10px 0;
+  padding: 10px;
+}
+</style>
